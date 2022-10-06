449ffdb1e8881aee8ca801727e5a74e401588c5f
Patrick Gentner <patrickgentner@gmx.de>
2022-10-06 13:44:23 +0200

    EnableBasicSettingsV2

--- EnableBasicSettings.ps1
+++ EnableBasicSettings.ps1
@@ -1,4 +1,5 @@
 function EnableWinRM {
+    #Prüfung ob WinRM aktiv ist
     Write-Host -ForegroundColor Cyan "WinRM Pruefung wird durchgefuehrt..."
     $WinrmStatus = (Get-Service WinRM).Status
     if($WinrmStatus -eq "Running"){
@@ -31,6 +32,7 @@ function DisableSMB1{
 }
 
 function SetExecutionPolicy{
+    #Prüfung ob ExecutionPolicy auf "Bypass" gesetzt ist
     Write-Host -ForegroundColor Cyan "ExecutionPolicy Pruefung wird durchgefuehrt!"
     $ExecutionPolicyStatus = Get-ExecutionPolicy
     if($ExecutionPolicyStatus -eq "Bypass"){
@@ -45,8 +47,55 @@ function SetExecutionPolicy{
 }
 
 
+function JoinADDomain{
+    #DomainJoin
+    [CmdletBinding()]
+Param(
+   $DomName = @( "contoso.local")
+)
+ 
+Begin 
+{
+    Write-Host -ForegroundColor Cyan "Domainanmeldung wird gestartet..."   
+}
+ 
+Process 
+{
+    Write-Host -NoNewline -ForegroundColor Green "Moechten Sie diesen Computer zu einer Domain hinzufuegen (J/N)"
+    $UserInput1 = Read-Host
+    if($UserInput1 -eq "j" -or $UserInput1 -eq "J"){
+        $ComputerHostName = $env:COMPUTERNAME
+        Write-Host -NoNewline -ForegroundColor Green "Moechten Sie dem Computer einen neuen Hostnamen geben (J/N)"
+        $UserInput2 = Read-Host
+        if($UserInput2 -eq "j" -or $UserInput2 -eq "J"){
+            Write-Host -NoNewline -ForegroundColor Green "Bitte geben Sie den neuen Hostname ein: "
+            $ComputerHostName = Read-Host
+            Write-Host -NoNewline -ForegroundColor Green "Bitte geben Sie Ihre Zugangsdaten ein:"
+            $DomainCredentials = Get-Credential
+            Write-Host""
+            Write-Host -ForegroundColor Cyan "Der Computer " $ComputerHostName "wird der Domain " $DomName "hinzugefuegt..."
+            Add-Computer -ComputerName $ComputerHostName -DomainName $DomName -Credential $DomainCredentials -Restart -Force
+        }
+        else {
+            Write-Host -NoNewline -ForegroundColor Green "Bitte geben Sie Ihre Zugangsdaten ein:"
+            $DomainCredentials = Get-Credential
+            Write-Host""
+            Write-Host -ForegroundColor Cyan "Der Computer " $ComputerHostName "wird der Domain " $DomName "hinzugefuegt..."
+            Add-Computer -ComputerName $ComputerHostName -DomainName $DomName -Credential $DomainCredentials -Restart -Force
+        }
+    }   
+    else{
+        Write-Host -NoNewline -ForegroundColor Green "Domainanmeldung wird beendet..."
+    }
+}
+ 
+}
+
+
+#Funktionsaufruf
 SetExecutionPolicy
 EnableWinRM
 DisableSMB1
+JoinADDomain
 
 
