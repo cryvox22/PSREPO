bddc839f910ed0be68b78dd23a4c7ae30a64bd02
Patrick Gentner <patrickgentner@gmx.de>
2022-10-11 15:06:26 +0200

    Get-FolderFilesV1

---
+++ Get-FolderFiles.ps1
@@ -0,0 +1,32 @@
+function GetFolderFiles{
+    #Abfrage des Pfades
+    Write-Host -ForegroundColor Cyan -NoNewline "Bitte gib den Pfad des Ordners ein: "
+    $FolderPath = Read-Host
+    Write-Host -ForegroundColor Cyan -NoNewline "Sollen Unterordner mit eingeschlossen werden? (J/N): "
+    $UserInput1 = Read-Host
+    if($UserInput1 -eq "j" -or $UserInput1 -eq "J"){
+        Write-Host -ForegroundColor Green "Hier deine Auflistung: "
+        Get-ChildItem -Path $FolderPath -Recurse | Select-Object Name, DirectoryName | Format-Table -AutoSize
+    }
+    else{
+        Write-Host -ForegroundColor Green "Hier deine Auflistung: "
+        Get-ChildItem -Path $FolderPath | Select-Object Name, DirectoryName | Format-Table -AutoSize
+    }
+
+
+    Write-Host -ForegroundColor Green "Hier deine Auflistung: "
+    Get-ChildItem -Path $FolderPath | Select-Object Name, DirectoryName | Format-Table -AutoSize
+
+    Write-Host -ForegroundColor Cyan -NoNewline "Moechtest du einen weiteren Pfad prüfen? (J/N) :"
+    $UserInput2 = Read-Host
+    if($UserInput2 -eq "j" -or $UserInput2 -eq "J"){
+        Clear-Host
+        GetFolderFiles
+    }else{
+        exit
+    }
+
+    
+}
+
+GetFolderFiles
