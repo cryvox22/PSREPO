5d59406dd06329e1554ef554e3eb38e369de2706
Patrick Gentner <patrickgentner@gmx.de>
2022-10-12 10:27:19 +0200

    PushPSRepoToInstallerV2

--- DisableAutoServer-Reboot.ps1
+++ DisableAutoServer-Reboot.ps1
@@ -1,13 +1,13 @@
 function DisableAutoServerReboot{
 #Documentation: https://it-learner.de/die-aufgabeplanung-mit-der-windows-powershell-verwalten/
 #Documentation: https://www.mioso.com/blog/microsoft-windows/windows-server/automatischer-neustart-nach-updates-deaktivieren-windows-server-2016/
-    Write-Host -ForegroundColor Cyan "Die Aufgabe für den automatischen Reboot wird deaktiviert..."
+    Write-Host -ForegroundColor Cyan "Die Aufgabe fuer den automatischen Reboot wird deaktiviert..."
     Get-ScheduledTask -TaskPath \Microsoft\Windows\UpdateOrchestrator* -TaskName Reboot | Disable-ScheduledTask
     $LocalHostname = $env:computername
     $TaskFolder = "C:\Windows\System32\Tasks\Microsoft\Windows\UpdateOrchestrator\Reboot"
     $NewFileName = "Reboot.bak"
     Rename-Item -Path $TaskFolder -NewName $NewFileName
-    Write-Host -ForegroundColor Green "Der automatische Server-Reboot wurde für $LocalHostname deaktiviert!"
+    Write-Host -ForegroundColor Green "Der automatische Server-Reboot wurde fuer $LocalHostname deaktiviert!"
 
 }
 
