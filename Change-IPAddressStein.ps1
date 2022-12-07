6299d82abd494a668fc4fe9a47edb357c9afaa50
Patrick Gentner <patrickgentner@gmx.de>
2022-12-07 11:02:32 +0100

    Change-IPAddressSteinV2

--- Change-IPAddressStein.ps1
+++ Change-IPAddressStein.ps1
@@ -2,7 +2,7 @@
 #Change-IPAddressStein
 function ChangeIPAddress{
 
-$InterfaceIndex = 0
+$InterfaceIndex = 5
 
 Write-Host -ForegroundColor Cyan "Dieses Tool hilft Ihnen ihre IP-Adresse passend für die Siemens SmartClient anzupassen!"
 Write-Host ""
@@ -10,6 +10,7 @@ Write-Host -ForegroundColor Cyan "Folgende Adressen haben Sie zur Auswahl:"
 Write-host -ForegroundColor Green "[1] - PIA Hauptmaschine - 192.168.0.55"
 Write-host -ForegroundColor Green "[2] - PIA Hauptmaschine - 192.168.0.8"
 Write-host -ForegroundColor Green "[3] - PIA Hauptmaschine - 192.168.0.9"
+Write-host -ForegroundColor Green "[4] - Standard (Automatisch)"
 Write-Host ""
 Write-Host -NoNewline -ForegroundColor Cyan "Bitte geben Sie die entsprechende Nummer ein: "
 $Input1 = Read-Host
@@ -29,6 +30,12 @@ switch ($Input1)
     New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress 192.168.0.9 -PrefixLength 24
     Write-Host -ForegroundColor Cyan "Die IP-Adresse wurde angepasst!"
     ; Break}
+
+4{
+    Set-NetIPInterface -InterfaceIndex $InterfaceIndex  -Dhcp Enabled
+    Write-Hoste -ForegroundColor Cyan "Die IP-Adresse wurde wieder auf Standard gesetzt!"
+
+}
 
 Default {    Write-Host -ForegroundColor Cyan "Keine Übereinstimmung" }
 
