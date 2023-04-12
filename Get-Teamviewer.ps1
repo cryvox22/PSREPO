f343138a66a9aa060e7f76cd1bfde7608767fd66
Patrick Gentner <patrickgentner@gmx.de>
2023-04-12 17:46:05 +0200

    Get-TeamViewerV3

--- Get-Teamviewer.ps1
+++ Get-Teamviewer.ps1
@@ -7,6 +7,13 @@ write-host "`n"
 
 $DownloadURL = "https://slsvde-my.sharepoint.com/:f:/g/personal/p_gentner_sl-sv_de/Eq5avOtKgE9AnErMrNvooYsB0yUaK196mGYk9xVBlohFEg?e=sg3HJq"
 
-Invoke-WebRequest -Uri $DownloadURL -OutFile TeamViewer.msi
-#/qn APITOKEN=19910524-dWzmY4xQ3FvO8WbjoOyG CUSTOMCONFIGID=6834ixj ASSIGNMENTOPTIONS="--alias %ComputerName% --grant-easy-access"
+
+
+$InstallFilePath = Invoke-WebRequest -Uri $DownloadURL -OutFile TeamViewer.msi
+
+$Argumentlist = '/I C:\TeamViewer.msi /passive APITOKEN=19910524-dWzmY4xQ3FvO8WbjoOyG CUSTOMCONFIGID=6834ixj ASSIGNMENTOPTIONS="--alias %ComputerName% --grant-easy-access"'
+
+Start-Process msiexec.exe -Wait -ArgumentList $Argumentlist
+
+#.\TeamViewer.msi /qn APITOKEN=19910524-dWzmY4xQ3FvO8WbjoOyG CUSTOMCONFIGID=6834ixj ASSIGNMENTOPTIONS="--alias %ComputerName% --grant-easy-access"
 Read-Host
