1f07a5949e5fff79e6458d0a4ff708b8b9f01435
Patrick Gentner <patrickgentner@gmx.de>
2023-04-13 10:49:11 +0200

    Connect-ServerEyeV2

--- Connect-ServerEye.ps1
+++ Connect-ServerEye.ps1
@@ -5,6 +5,8 @@ write-host "`n"
 Write-Host -ForegroundColor Blue "Der TeamViewer wird installiert..."
 write-host "`n"
 
+#https://servereye.freshdesk.com/support/solutions/articles/14000070083-installation-powershell-helper-modul
+#https://servereye.freshdesk.com/support/solutions/articles/14000088986-anlegen-einer-alarmierung-bei-allen-sensoren-eines-kunden-mit-verz%C3%B6gerung-und-filterung-nach-tags-
 
 
 if (!(Get-Module "ServerEye.Powershell.Helper")) {
@@ -12,5 +14,19 @@ if (!(Get-Module "ServerEye.Powershell.Helper")) {
     Install-Module -Name ServerEye.Powershell.Helper
 }
 Import-Module -Name ServerEye.Powershell.Helper
+Update-SEHelper
 
 Connect-SESession -persist
+
+
+Write-Host -ForegroundColor Blue "Liste Kunden auf..."
+Get-SECustomer
+
+#Write-Host -ForegroundColor Blue -NoNewline "Moechtest du eine Alarmierung hinzufügen? (J/N)"
+#$input1 = Read-Host 
+#if($input1 -eq "j" -or $input1 -eq "J"){
+#    Write-Host -ForegroundColor Blue -NoNewline "Bitte gib den Kundennamen ein"
+#    $inputCustomer = Read-Host
+#    Get-SECustomer -Filter $inputCustomer | Get-Sensor -Filter * | New-SENotification -UserID
+#}
+
