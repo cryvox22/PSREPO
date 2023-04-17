aac99e4ef9dfce8105b9f2a05ec5ac7f90c6951a
Patrick Gentner <patrickgentner@gmx.de>
2023-04-17 14:03:48 +0200

    Start3cxV1

--- Start-3CX.ps1
+++ Start-3CX.ps1
@@ -9,7 +9,7 @@ function start3CX{
     Start-Service -Name *3cx*
     Get-Service -Name *3cx* | Select-Object Name, Status  
 }
-
+start3CX
 Write-Host -ForegroundColor Cyan  "3CX-Dienste wurden gestartet...!"
 Write-Host -ForegroundColor Red -NoNewline "Bitte eine beliebige Taste druecken um zu beenden..."
 Read-Host
