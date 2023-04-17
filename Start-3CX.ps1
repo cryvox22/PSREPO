f7732c772f0c03b352ba208846e89aea1c305fc1
Patrick Gentner <patrickgentner@gmx.de>
2023-04-17 14:26:14 +0200

    Start3cxV1

--- Start-3CX.ps1
+++ Start-3CX.ps1
@@ -1,15 +1,20 @@
-write-host "`n"
-Write-Host -ForegroundColor Green "Willkommen beim Start-3CX!"
-Write-Host "------------------------------------"
-write-host "`n"
-Write-Host -ForegroundColor Cyan  "3CX-Dienste werden gestartet...!"
+
 
 
 function start3CX{
-    Start-Service -Name *3cx*| Where-Object{$_.StartType -eq "Automatic"}
-    Get-Service -Name *3cx* | Select-Object Name, Status  
-}
-start3CX
-Write-Host -ForegroundColor Cyan  "3CX-Dienste wurden gestartet...!"
-Write-Host -ForegroundColor Red -NoNewline "Bitte eine beliebige Taste druecken um zu beenden..."
-Read-Host
+    write-host "`n"
+    Write-Host -ForegroundColor Green "Willkommen beim Start-3CX!"
+    Write-Host "------------------------------------"
+    write-host "`n"
+    Write-Host -ForegroundColor Cyan  "3CX-Dienste werden gestartet...!"
+    Start-Service -Name *3cx* | Where-Object{$_.StartType -eq "Automatic"}
+    
+    Write-Host -ForegroundColor Cyan  "3CX-Dienste wurden gestartet...!"
+    Get-Service -Name *3cx* | Where-Object{$_.StartType -eq "Automatic"} | Select-Object Name, Status  
+    
+    Write-Host -ForegroundColor Red -NoNewline "Bitte eine beliebige Taste druecken um zu beenden..."
+    Read-Host
+    }
+    
+    start3CX
+    
