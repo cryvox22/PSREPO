14b7ea9dc0916fa1b8b7d7a208b1800b57ceeea9
Patrick Gentner <patrickgentner@gmx.de>
2023-04-25 16:17:31 +0200

    First Half-Solid Publish for SLSHellV1

--- Module/SLModule/SLShell.psm1
+++ Module/SLModule/SLShell.psm1
@@ -1,3 +1,27 @@
+<#
+
+Eigene Read- und Write-Function schreiben, die in den Untermodulen verwendet werden kann
+
+
+
+ToDo's für die Untermodule: 
+
+SL-Doc: 
+- alle Teilbereiche in eigene Functions auslagern
+- Ausgabe und Zusammenfassung verbessern
+- Systemspecs mit auslesen
+- Get-Mac anpassen
+- einzelne Teilinformationen via. Befehl abrufbar machen (z.B. SL-Doc -Value "Serial" -> gibt Seriennummer aus und schiebt sie ins Clipboard)
+
+
+
+
+#>
+
+
+
+
+
 function ModuleStarted{
     Clear-Host
     Write-Host -ForegroundColor Cyan "-----Willkommen zur SLShell!-----"
@@ -17,9 +41,9 @@ function ModuleStarted{
 
 function Doc{
 #----------------------
+
 #Variablen: 
 #----------------------
-
 #----------------------
 #Programm
 #----------------------
@@ -28,16 +52,12 @@ Clear-Host
 write-host "`n"
 Write-Host "Welcome to SL-Doc" 
 Write-Host "-------------------------------------------"
-write-host "`n"
-Write-Host "Wir werden nacheinander alle benötigten Systemdaten auslesen und in Ihre Zwischenablage kopieren!" 
-
-write-host "`n"
+Write-Host "Wir werden nacheinander alle relevanten Systemdaten auslesen und in Ihre Zwischenablage kopieren!" 
 write-host "`n"
 
 #----------------------
 #Hostname
 #----------------------
-
 Write-Host "-------------------------------------------"
 Write-Host "Hostnamen: "
 Write-Host "-------------------------------------------"
@@ -49,18 +69,14 @@ Write-Host -NoNewline -ForegroundColor Yellow $hostname
 Write-Host " befindet sich jetzt in der Zwischenablage!"
 Write-Host "`n"
 pause
-Write-Host "`n"
 
 #----------------------
 #Seriennummer
 #----------------------
-
 write-host "`n"
 Write-Host "-------------------------------------------"
 Write-Host "Seriennummer: "
 Write-Host "-------------------------------------------"
-write-host "`n"
-
 $serial = (Get-CimInstance -ClassName Win32_BIOS -Property SerialNumber).SerialNumber
 $serial | clip
 write-host "`n"
@@ -71,20 +87,17 @@ Write-Host -NoNewline -ForegroundColor Yellow $serial
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 write-host "`n"
 pause
-write-host "`n"
+
 
 #----------------------
 #Modell
 #----------------------
-
 write-host "`n"
 Write-Host "-------------------------------------------"
 Write-Host "Modell: "
 Write-Host "-------------------------------------------"
 write-host "`n"
-
 $system = Get-CimInstance CIM_ComputerSystem
-
 $Modell = $system.Model
 $Modell | clip
 write-host "`n"
@@ -95,12 +108,10 @@ Write-Host -NoNewline -ForegroundColor Yellow $Modell
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 write-host "`n"
 pause
-write-host "`n"
 
 #----------------------
 #Installiert am: 
 #----------------------
-
 write-host "`n"
 Write-Host "-------------------------------------------"
 Write-Host "Installiert am: "
@@ -108,22 +119,18 @@ Write-Host "-------------------------------------------"
 write-host "`n"
 write-host "`n"
 Write-Host -NoNewline "Das Device " $hostname " wurde am folgenden Tag installiert: "   
-        
 $installdate = (Get-WmiObject Win32_OperatingSystem).ConvertToDateTime( (Get-WmiObject Win32_OperatingSystem).InstallDate ) 
-      
 Write-Host -NoNewline -ForegroundColor Yellow $installdate
 write-host "`n"
 $installdate | clip
-
 Write-Host "Das Installationsdatum befindet sich jetzt in der Zwischenablage!"
 write-host "`n"
 pause
-write-host "`n"
+
 
 #----------------------
 #Hostname
 #----------------------
-
 Write-Host "-------------------------------------------"
 Write-Host "Hostnamen: "
 Write-Host "-------------------------------------------"
@@ -138,40 +145,33 @@ Write-Host "`n"
 #----------------------
 #IP-Addresse
 #----------------------
-
 Write-Host "-------------------------------------------"
 Write-Host "IP-Addresse: "
 Write-Host "-------------------------------------------"
 Write-Host "`n"
-
 $ip =  (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).ipaddress
 $ip | clip
-
 Write-Host -NoNewline "Die IP-Addresse " 
 Write-Host -NoNewline -ForegroundColor Yellow $ip 
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 Write-Host "`n"
 pause
-write-host "`n"
+
 
 
 #----------------------
 #Mac-Addresse
 #----------------------
-
 Write-Host "-------------------------------------------"
 Write-Host "Mac-Addresse: "
 Write-Host "-------------------------------------------"
 Write-Host "`n"
 Get-NetAdapter 
-
 $EthAdapterNumber = Read-Host "Suche einen Adapter aus (1,2,3..)"
 write-host "`n"
-
 $intEthAdapterNumber = [int]$EthAdapterNumber
 $intEthAdapterNumber = $intEthAdapterNumber-1
 $AdapterName = "Ethernet" +$intEthAdapterNumber
-
 if($intEthAdapterNumber-eq 0)
 {
 $mac =  (Get-NetAdapter -Name Ethernet).macaddress
@@ -182,31 +182,27 @@ else
 $mac =  (Get-NetAdapter -Name Ethernet + $intEthAdapterNumber).macaddress
 $mac | clip
 }
-
 Write-Host -NoNewline "Die Mac-Addresse " 
 Write-Host -NoNewline -ForegroundColor Yellow $mac
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 write-host "`n"
 pause
-Write-Host "`n"
+
 
 #----------------------
 #Gateway
 #----------------------
-
 Write-Host "-------------------------------------------"
 Write-Host "Gateway: "
 Write-Host "-------------------------------------------"
 write-host "`n"
 $gateway = Get-NetRoute -DestinationPrefix "0.0.0.0/0" | Select-Object -ExpandProperty "NextHop"
 $gateway | clip
-
 Write-Host -NoNewline "Das Gateway  " 
 Write-Host -NoNewline -ForegroundColor Yellow $gateway 
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 write-host "`n"
 pause
-write-host "`n"
 
 #----------------------
 #OS
@@ -216,16 +212,14 @@ Write-Host "-------------------------------------------"
 Write-Host "Betriebssystem: "
 Write-Host "-------------------------------------------"
 write-host "`n"
-
 $os = (Get-WMIObject win32_operatingsystem).caption
 $os | clip
-
 Write-Host -NoNewline "Die Betriebssystemversion " 
 Write-Host -NoNewline -ForegroundColor Yellow $os 
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 write-host "`n"
 pause
-write-host "`n"
+
 
 #----------------------
 #Windows-Key
@@ -234,50 +228,36 @@ Write-Host "-------------------------------------------"
 Write-Host "Windows-Key: "
 Write-Host "-------------------------------------------"
 Write-Host "`n"
-
 $WindowsKey = (Get-WmiObject SoftwareLicensingService).OA3xOriginalProductKey
 $WindowsKey | clip
-
 Write-Host -NoNewline "Der Windows-Key " 
 Write-Host -NoNewline -ForegroundColor Yellow $WindowsKey 
 Write-Host " befindet sich jetzt in Ihrer Zwischenablage!"
 Write-Host "`n"
 pause
 write-host "`n"
-
 Clear-Host
 Write-Host -ForegroundColor Yellow "Zusammenfassung:"
-
 Write-Host -ForegroundColor Yellow "Hostname:"
 Write-Host $hostname "`n"
-
 Write-Host -ForegroundColor Yellow "Seriennummer:"
 Write-Host $serial "`n"
-
 Write-Host -ForegroundColor Yellow "Modell:" 
 Write-Host $Modell "`n"
-
 Write-Host -ForegroundColor Yellow "Installiert am:"
 Write-Host $installdate "`n"
-
 Write-Host -ForegroundColor Yellow "IP-Adresse:"
 Write-Host $ip "`n"
-
 Write-Host -ForegroundColor Yellow "Mac-Adresse:"
 Write-Host $mac "`n"
-
 Write-Host -ForegroundColor Yellow "Gateway:"
 Write-Host $gateway "`n"
-
 Write-Host -ForegroundColor Yellow  "Betriebssystem:"
 Write-Host $os "`n"
-
 Write-Host -ForegroundColor Yellow "Windows-Key:"
 Write-Host $WindowsKey "`n"
-
 write-host "`n"
 Write-Host "End of SL-Doc"
-
 }
 
 function Cleanup{
