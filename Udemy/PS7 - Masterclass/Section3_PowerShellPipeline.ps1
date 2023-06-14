45e9cfe3c12f4dbdc64541f90e83a28884e9bb8f
Patrick Gentner <patrickgentner@gmx.de>
2023-06-14 14:51:00 +0200

    Section3V1 - 14.06.2023

---
+++ Udemy/PS7 - Masterclass/Section3_PowerShellPipeline.ps1
@@ -0,0 +1,16 @@
+Write-Host "Hier geht es um Pipelines!" -ForegroundColor Green
+
+
+#Get-Process notepad mspaint | Stop-Process
+
+#Get-SmbOpenFile | Close-SmbOpenFile
+
+#New-Item -ItemType File -Path .\usernames.txt
+
+Add-Content -Path .\usernames.txt -Value "Patrick", "Gentner", "Rudolf", "Gentner", "Birgit", "Gentner", "Birgit", "Gentner"
+
+#Get-Content -Path .\usernames.txt
+#(Get-Content -Path .\usernames.txt)[0..2]
+
+Get-Content -Path .\usernames.txt | Sort-Object | Get-Unique | Add-Content -Path .\usernames_unique.txt
+Get-Content -Path .\usernames_unique.txt
