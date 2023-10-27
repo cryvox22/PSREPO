f66ebeb2aff5f63c59d7fe2242b78559530eb949
Patrick Gentner <patrickgentner@gmx.de>
2023-10-27 11:15:28 +0200

    Changed some minor Settings in the new-psmodule.psm1

--- Module/New-PSModule/New-PSModule.psm1
+++ Module/New-PSModule/New-PSModule.psm1
@@ -22,20 +22,21 @@ function New-PSModule {
             Position = 0,
             ValueFromPipeline = $true,
             ValueFromPipelineByPropertyName = $true,
-            HelpMessage = 'Provide your Modulename'
+            HelpMessage = 'Provide your Modulename'            
         )]
         [string]
         $ModulName
     )
 
     $ModulManifestSettings = @{
-        Path = (Get-Location)
+        Path = "$(Get-Location)$($ModulName).psd1"
         GUID = (New-Guid)
         Author = $env:USERNAME
         CompanyName = "Default"
         RootModule = (New-Item -Name "$($ModulName).psm1" -ItemType File).Name
         ModuleVersion = "0.0.1"
         Description = "This is a brandnew Manifest"
+        Copyright = '(c) Patrick Gentner. All rights reserved.'
     }
 
     New-ModuleManifest @$ModulManifestSettings
