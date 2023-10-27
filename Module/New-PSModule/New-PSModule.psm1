29db4007d89f2c5bf7faeff75183e15c61f19289
Patrick Gentner <patrickgentner@gmx.de>
2023-10-27 11:00:42 +0200

    Added New-PSModule Module

---
+++ Module/New-PSModule/New-PSModule.psm1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    New-PSModule Creation with psm1 and psd1 Files
+.DESCRIPTION
+    This Function creates a fresh new Module for you
+.NOTES
+    This is written on PowerShell 7
+.LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+.PARAMETER ModulName [string]
+    This is the name used for the new Module psd1 & psm1 Files
+.EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+#>
+function New-PSModule {
+    [CmdletBinding()]
+    param(
+        # ModuleName
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            HelpMessage = 'Provide your Modulename'
+        )]
+        [string]
+        $ModulName
+    )
+
+    $ModulManifestSettings = @{
+        Path = (Get-Location)
+        GUID = (New-Guid)
+        Author = $env:USERNAME
+        CompanyName = "Default"
+        RootModule = (New-Item -Name "$($ModulName).psm1" -ItemType File).Name
+        ModuleVersion = "0.0.1"
+        Description = "This is a brandnew Manifest"
+    }
+
+    New-ModuleManifest @$ModulManifestSettings
+}
